1edb8d16570fb10580a375332698f91913976365
hiteshh456 <91420226+hiteshh456@users.noreply.github.com>
2021-10-30 23:04:12 +0530

    Add files via upload
    GCD

---
+++ GCDD/gcd.java
@@ -0,0 +1,19 @@
+package GCDD;
+
+public class gcd {
+    public static void main(String[] args)
+    {
+
+        int x = 10, y = 6, gcd = 1;
+
+        for(int i = 1; i <= x && i <= y; i++)
+        {
+
+            if(x%i==0 && y%i==0)
+
+                gcd = i;
+        }
+
+        System.out.printf("GCD of %d and %d is: %d", x, y, gcd);
+    }
+}
